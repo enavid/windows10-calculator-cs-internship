462eeaf7a65e9e2dc9cab83b9814abe358549dba
enavid <en.navid@outlook.com>
2021-09-09 19:05:38 +0430

    handle eventlistenr in controller

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,9 @@
 import _display from './display.js';
+import _keyBoard from './keyboard.js';
 
 
 _display.render(100)
+
+_keyBoard.addEventListener('number', (number) => {
+    console.log('controller', number);
+})
--- js/keyboard.js
+++ js/keyboard.js
@@ -1,15 +1,20 @@
 const get = document.getElementById.bind(document);
 const keyboard = get('keyboard');
+const _eventHandler = {};
 
 //================================ Keyboard API ===========================
-export default {};
+export default { addEventListener };
 
 //=========================== Keyboard event listener ====================
 console.log(keyboard)
 keyboard.addEventListener('click', (e) => {
     e.preventDefault();
-    const event = e.target.getAttribute('value');
-    console.log(event)
+    const number = e.target.getAttribute('value');
+    if (_eventHandler['number']) _eventHandler['number'](number);
 })
 
 //=========================== Define keyboard function ===================
+
+function addEventListener(option, callBack) {
+    _eventHandler[option] = callBack;
+}
