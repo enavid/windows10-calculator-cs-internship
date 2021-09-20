db6c2a9e879819cbc9fddb1904fff665cdb85e44
enavid <en.navid@outlook.com>
2021-09-20 22:22:37 +0430

    handle single operator

--- js/controller.js
+++ js/controller.js
@@ -1,103 +1,36 @@
 import _display from './display.js';
 import _keyBoard from './keyboard.js';
 
+let controller_state = 0;
 let first_number = '';
 let second_number = '';
-let sign = '';
 
 //=========================== control event listener ====================
 _keyBoard.addEventListener('number', (number) => {
-    if (first_number == '' && number == 0) return;
-    first_number = first_number + number;
-    console.log(first_number);
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('dot', () => {
-    if (first_number.includes('.') || first_number == '') return;
-    first_number = first_number + '.';
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('back', () => {
-    first_number = first_number.slice(0, -1);
-    if (first_number == '') { return _display.renderZero() };
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('clear', () => {
-    first_number = '';
-    _display.renderZero();
-})
-
-_keyBoard.addEventListener('eclear', () => {
-    first_number = '';
-    _display.renderZero();
-})
-
-_keyBoard.addEventListener('sqrt', () => {
-    first_number = Math.sqrt(first_number).toString();
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('power', () => {
-    first_number = Math.pow(first_number, 2).toString();
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('oneDivision', () => {
-    if (first_number === 0) return;
-    first_number = 1 / first_number;
-    _display.render(first_number);
-})
-
-_keyBoard.addEventListener('multiply', () => {
-    toggleNumber();
-    sign = '*';
-})
-
-_keyBoard.addEventListener('division', () => {
-    toggleNumber();
-    sign = '/';
-})
-
-_keyBoard.addEventListener('sum', () => {
-    toggleNumber();
-    sign = '+';
-})
-
-_keyBoard.addEventListener('minus', () => {
-    toggleNumber();
-    sign = '-';
-})
-
-_keyBoard.addEventListener('equal', () => {
-    switch (sign) {
-        case '*': {
-            first_number = first_number * second_number;
-            break;
-        }
-        case '/': {
-            if (first_number == 0) return;
-            first_number = second_number / first_number;
-            break;
-        }
-        case '+': {
-            first_number = parseInt(first_number) + parseInt(second_number);
-            break;
-        }
-        case '-': {
-            first_number = parseInt(second_number) - parseInt(first_number);
-            break;
-        }
+    if (controller_state === 0) {
+        first_number = number;
+        console.log(first_number);
     }
-    _display.render(first_number);
+    if (controller_state === 3) {
+        second_number = number;
+        console.log(second_number);
+    }
+})
+
+_keyBoard.addEventListener('single_operator', (operator) => {
+    controller_state = 0; // check it and add return to if
+
+    if (operator === 'sqrt') first_number = Math.sqrt(first_number);
+    if (operator === 'power') first_number = Math.pow(first_number, 2);
+    if (operator === 'oneDivision' && first_number != 0) first_number = 1 / first_number;
     console.log(first_number)
 })
 
 // ========================== Define control function ==================
 
-function toggleNumber() {
-    second_number = first_number;
-    first_number = '';
+
+
+function twice_operator(value) {
+    const operator = '*/+-';
+    return operator.includes(value);
 }
