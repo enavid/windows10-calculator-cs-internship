737ef18a51829a89e901ab761bdaac2de389eb41
enavid <en.navid@outlook.com>
2021-11-19 10:14:17 +0330

    create simple test server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
