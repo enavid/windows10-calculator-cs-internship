71642965aaaddf0658e75c2c733bec966bb193fc
enavid <en.navid@outlook.com>
2021-11-19 12:23:24 +0330

    used MVC pattern and create controller.js

---
+++ server/controller.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function indexHtml(req, res) {
+    readFile({ url: '/index.html' }, res);
+}
+
+function readFile(req, res) {
+    var filePath = path.join(__dirname, '../client/', req.url);
+
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            res.writeHead(404);
+            res.write('Not found error 404');
+            res.end()
+        } else {
+            if (req.url.includes('js')) {
+                res.setHeader('Content-Type', 'application/javascript')
+            }
+            res.writeHead(200);
+            res.write(data);
+            res.end();
+        }
+    })
+}
+
+module.exports = indexHtml;
